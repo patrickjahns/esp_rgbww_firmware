c71c0ded87367f33a3e193cfe00973b0dc85b206
Patrick Jahns <github@patrickjahns.de>
2016-03-12 12:18:26 +0100

    version.json cleanup

--- deploy.sh
+++ deploy.sh
@@ -39,26 +39,13 @@ cd $TRAVIS_BUILD_DIR/release
 
 # create version information
 cat <<EOF > version.json
-{"rom" : {
-	"version": "${FW_VERSION}",
-	"url": "${GH_PAGE_LINK}rom0.bin"
-	},
- "webapp": {
- 	"version": "${WEBAPP_VERSION}",
- 	"url": [
- 	"${GH_PAGE_LINK}/init.html.gz",
- 	"${GH_PAGE_LINK}/index.html.gz",
- 	"${GH_PAGE_LINK}/app.min.css.gz",
- 	"${GH_PAGE_LINK}/app.min.js.gz"
- 	]
- }
-}
+{"rom":{"version":"${FW_VERSION}","url":"${GH_PAGE_LINK}rom0.bin"},"webapp":{"version":"${WEBAPP_VERSION}","url":["${GH_PAGE_LINK}/init.html.gz","${GH_PAGE_LINK}/index.html.gz","${GH_PAGE_LINK}/app.min.css.gz","${GH_PAGE_LINK}/app.min.js.gz"]}}
 EOF
 
 mkdir -p $TRAVIS_BUILD_DIR/_release
 cd $TRAVIS_BUILD_DIR/_release 
 
-#Push to github
+#Push to github pages
 git init
 git remote add upstream "https://$GH_TOKEN@${GH_REF}"
 git fetch upstream
@@ -70,5 +57,4 @@ mv $TRAVIS_BUILD_DIR/release $TRAVIS_BUILD_DIR/_release/
 touch .
 git add .
 git commit -m "Release Firmware v${FW_VERSION} webapp v${WEBAPP_VERSION}"
-
 git push -q upstream HEAD:gh-pages
